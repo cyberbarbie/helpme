3539e6792fa3aa82c585644adf3ed65d32de0d67
Tae'lur Alexis <taeluralexis@gmail.com>
2023-10-04 17:23:32 -0400

    Update RPSvscomputer.py

--- RPSvscomputer.py
+++ RPSvscomputer.py
@@ -1,29 +1,30 @@
 from random import randint
 
-
 print("Welcome to the Ultimate Rock, Paper, Scissors Game \n I hope you are ready \n...ROCK...\n...PAPER...\n...SCISSORS...")
-Player1 = input("(enter Player 1's choice):")
-Player2 = randint(1,3)
+Player1 = input("(enter Player 1's choice): ")
+Player2 = randint(1, 3)
 print("Shoot!!!!")
-1 is "rock"
-2 is "scissors"
-3 is "paper" 
-if Player1 == Player2: 
-        print("You guys are evenly matched , Its a tie")
-elif Player1 == "rock": 
-    if Player2 == "scissors":
-        print("Player 1 wins")
-    elif Player2 == "paper":
-        print("Player 2 wins")
-elif Player1 == "paper": 
-    if Player2 == "scissors":
-        print("Player 2 wins")
-    elif Player2 == "rock":
-        print("Player 1 wins")  
-elif Player1 == "paper": 
-    if Player2 == "scissors":
-        print("Player 2 wins")
-    elif Player2 == "rock":
-        print("Player 1 wins") 
 
-else: print("Something went wrong")
+if Player1 == "rock":
+    if Player2 == 1:
+        print("You guys are evenly matched, It's a tie")
+    elif Player2 == 2:
+        print("Player 2 wins")
+    elif Player2 == 3:
+        print("Player 1 wins")
+elif Player1 == "paper":
+    if Player2 == 1:
+        print("Player 1 wins")
+    elif Player2 == 2:
+        print("You guys are evenly matched, It's a tie")
+    elif Player2 == 3:
+        print("Player 2 wins")
+elif Player1 == "scissors":
+    if Player2 == 1:
+        print("Player 2 wins")
+    elif Player2 == 2:
+        print("Player 1 wins")
+    elif Player2 == 3:
+        print("You guys are evenly matched, It's a tie")
+else:
+    print("Something went wrong")
